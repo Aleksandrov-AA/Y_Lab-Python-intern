a631d53ead8b8aa85254703e04ac869d1f369723
Aleksandrov-AA <sana115@mail.ru>
2022-07-12 01:01:58 +0300

    add task 3-1 homework-3

--- HomeWork-3/homework-3(3)/task-3-1.py
+++ HomeWork-3/homework-3(3)/task-3-1.py
@@ -1,1 +1,14 @@
+if __name__ == '__main__':
 
+    dictionary = dict()
+
+    def funcDecorator(func):
+        def wrapper(number):
+            if not dictionary.get(number):
+                dictionary[number] = func(number)
+            return dictionary[number]
+        return wrapper
+
+    @funcDecorator
+    def multiplier(number: int):
+        return number * 2
